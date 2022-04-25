c9a1d9c87a160448355ad4ab1f0e353866260ae3
Arturo <ahpareja@gmail.com>
2022-04-25 11:16:16 -0500

    add luxon date time

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Book from './modules/book.js';
 import Books from './modules/books.js';
+import displayTime from './modules/date.js';
 
 const div = document.querySelector('.books');
 const awesome = document.querySelector('.awesome');
@@ -70,14 +71,4 @@ contactBtn.addEventListener('click', () => {
   awesome.classList.remove('active');
 });
 
-const today = new Date();
-
-const date = `${today.getMonth() + 1}-${today.getDate()}-${today.getFullYear()}`;
-
-const hours = `${today.getHours()}:${today.getMinutes()}`;
-
-const dateTime = `${date}, ${hours}`;
-
-const time = document.getElementById('date');
-
-time.innerHTML = dateTime;
+displayTime();
---
+++ modules/date.js
@@ -0,0 +1,9 @@
+const displayTime = () => {
+    const time = document.getElementById('date');
+    const currentDate = luxon.DateTime.local().toLocaleString(
+      luxon.DateTime.DATETIME_FULL,
+    );
+    time.innerHTML = currentDate;
+  };
+  
+  export default displayTime;
